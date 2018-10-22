5307d816cd76bda2a102b8a0e138fa673e461bb2
ildaru <ildarugatu@yandex.ru>
2018-10-22 15:22:45 +0900

    Execute Easy and Normal

--- hw05_easy.py
+++ hw05_easy.py
@@ -7,42 +7,32 @@ import os
 import sys
 import shutil
 
-list_dir = ['dir_' + str(el) for el in range(1, 10)]
 
-
-# Создаем кучу папок
-def make_bunch():
+# Создаем папку
+def make_dir(name_dir=''):
     try:
-        for el in list_dir:
-            os.mkdir(os.path.join(os.getcwd(), el))
+        os.mkdir(os.path.join(os.getcwd(), name_dir))
+        print('Папка <<{}>> успешно создана'.format(name_dir))
+        return True
     except FileExistsError:
         print('Такая папка уже существует')
-
-    return print('Папки {} - {} созданы'.format(list_dir[0], list_dir[len(list_dir) - 1]))
-
-
-# Удаляем сщзданную кучу папок
-def del_bunch():
-    try:
-        for el in list_dir:
-            os.rmdir(os.path.join(os.getcwd(), el))
+        return False
     except Exception as cls:
-        print('Exeption class =', cls)
+        print('Ошибка создания папки:', cls)
+        return False
 
-    return print('Папки {} - {} удалены'.format(list_dir[0], list_dir[len(list_dir) - 1]))
 
+# Удаляем папку
+def del_dir(name_dir=''):
+    try:
+        os.rmdir(os.path.join(os.getcwd(), name_dir))
+        print('Папка <<{}>> успешно удалена'.format(name_dir))
+        return True
+    except Exception as cls:
+        print('Ошибка удаления:', cls)
+        return False
 
-# Вызов функций для сздания и удаления папок
-key = ''
-try:
-    while key != 'q':
-        key = input("Haжмите 1 чтобы создать папки, 2 чтобы удалить и 'q' чтобы выйти\n")
-        if key == '1':
-            make_bunch()
-        elif key == '2':
-            del_bunch()
-except Exception as cls:
-    print('Exeption class =', cls)
+# Запускаемый скрипт в конце файла
 
 
 # Задача-2:
@@ -50,6 +40,7 @@ except Exception as cls:
 
 def cur_dirs():
     list_dirs = []
+    result_list_dirs = []
 
     # Вытаскиваем имена папок через функцию os.walk
     for root, dirs, files in os.walk("."):
@@ -57,15 +48,20 @@ def cur_dirs():
             list_dirs.append(dirs)
 
     # Убираем всё лишнее, оставляя только папки
-    result_list_dirs = list_dirs[0]
-    print(result_list_dirs)
+    if list_dirs != []:
+        result_list_dirs = list_dirs[0]
+
+    return result_list_dirs
 
 
-cur_dirs()
-
+# Если это главный файл, то получаем список папок
+if __name__ == '__main__':
+    current_dirs = cur_dirs()
+    print('Список папок - {}'.format(current_dirs))
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+
 
 # Функция копирования файла
 def duplicate_file(filename):
@@ -85,4 +81,29 @@ def duplicate_file(filename):
 # Получаем имя текущего файла
 cur_name = sys.argv[0][sys.argv[0].rfind('/') + 1:]
 
-duplicate_file(cur_name)
+
+# Вызов главного скрипта для создания и удаления папок
+# Функция будет вызвана только если это главный исполняемый файл
+if __name__ == '__main__':
+    key = ''
+    try:
+        while key != 'q':
+            key = input("[1] - создать папки,\n"
+                        "[2] - удалить папки,\n"
+                        "[3] - дубликат исполняемого файла"
+                        "[q] - выйти\n"
+                        "-->> ")
+            list_dir = ['dir_' + str(el) for el in range(1, 10)]
+
+            if key == '1':
+                for el in list_dir:
+                    make_dir(el)
+
+            elif key == '2':
+                for el in list_dir:
+                    del_dir(el)
+
+            elif key == '3':
+                duplicate_file(cur_name)
+    except Exception as cls:
+        print('Ошибка:', cls)
--- hw05_normal.py
+++ hw05_normal.py
@@ -13,3 +13,63 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+import os
+import sys
+import hw05_easy as easy
+
+while True:
+    try:
+        key = input('[1] - Перейти в папку\n'
+                    '[2] - Просмотреть содержимое текущей папки\n'
+                    '[3] - Удалить папку\n'
+                    '[4] - Создать папку\n'
+                    '[q] - Выход\n'
+                    '-->> ')
+    except Exception as cls:
+        print('Ошибка ввода:', cls)
+
+    if key == '1':
+        print('Список текущих папок:', easy.cur_dirs())
+        try:
+            choose_dir = input('Выберите папку для перехода,\n'
+                               'или "b" для перехода на уровень вверх\n'
+                               '-->> ')
+        except Exception as cls:
+            print('Ошибка ввода:', cls)
+        if choose_dir == 'b':
+            try:
+                choose_dir = os.getcwd()[:os.getcwd().rfind('\\')]
+            except Exception as cls:
+                print('Ошибка перехода:', cls)
+        if os.path.isdir(choose_dir):
+            os.chdir(choose_dir)
+            print('Успешный переход в папку <<{}>>'.format(choose_dir))
+
+    elif key == '2':
+        print('Содержимое текущей папки:', os.listdir(), '\n')
+
+    elif key == '3':
+        print('Список текущих папок:', easy.cur_dirs())
+        try:
+            choose_dir = input('Выберите папку для удаления,\n'
+                               '-->> ')
+        except Exception as cls:
+            print('Ошибка ввода:', cls)
+        if os.path.isdir(choose_dir):
+            easy.del_dir(choose_dir)
+
+    elif key == '4':
+        print('Список текущих папок:', easy.cur_dirs())
+        try:
+            new_dir = input('Введите название новой папки,\n'
+                            '-->> ')
+        except Exception as cls:
+            print('Ошибка ввода:', cls)
+        easy.make_dir(new_dir)
+
+    elif key == 'q':
+        sys.exit()
+
+    else:
+        print('Неизвестный выбор')
